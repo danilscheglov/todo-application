bbda110e27a5fbd2c786e35362fb3f3f5f42de6a
Danil Scheglov <scheglovofficial@gmail.com>
2026-06-27 16:31:44 +0300

    feat: implemented file saving for storing tasks

--- src/main/java/ru/danilscheglov/repository/TodoRepository.java
+++ src/main/java/ru/danilscheglov/repository/TodoRepository.java
@@ -2,9 +2,11 @@ package ru.danilscheglov.repository;
 
 import ru.danilscheglov.model.Task;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 /**
  * @author Danil Scheglov
@@ -15,10 +17,15 @@ public class TodoRepository {
 
     private int idCounter = 1;
 
+    public TodoRepository() {
+        loadFromFile();
+    }
+
     public void createTask(Task task) {
         task.setId(idCounter);
         tasks.put(task.getId(), task);
         idCounter++;
+        saveToFile();
     }
 
     public Task getTaskById(int id) {
@@ -31,9 +38,58 @@ public class TodoRepository {
 
     public void updateTask(Task task) {
         tasks.put(task.getId(), task);
+        saveToFile();
     }
 
     public void deleteTaskById(int id) {
         tasks.remove(id);
+        saveToFile();
+    }
+
+    private void saveToFile() {
+        List<String> lines = new ArrayList<>();
+
+        for (Task task : tasks.values()) {
+            lines.add(task.getId() + "|" + task.getName() + "|" + task.getDescription() + "|" + task.isCompleted());
+        }
+
+        try {
+            Files.write(Paths.get("tasks.txt"), lines);
+        } catch (IOException e) {
+            System.out.println("[Ошибка] Ошибка при создании файла!");
+        }
+    }
+
+    private void loadFromFile() {
+        Path path = Paths.get("tasks.txt");
+        if (Files.exists(path)) {
+            try {
+                List<String> lines = Files.readAllLines(path);
+
+                int maxId = 0;
+                for (String line : lines) {
+                    String[] parts = line.split("\\|");
+
+                    int id = Integer.parseInt(parts[0]);
+                    String name = parts[1];
+                    String description = parts[2];
+                    boolean isCompleted = Boolean.parseBoolean(parts[3]);
+
+                    Task task = new Task(name, description);
+                    task.setId(id);
+                    task.setCompleted(isCompleted);
+
+                    tasks.put(task.getId(), task);
+
+                    if(id > maxId) {
+                        maxId = id;
+                    }
+
+                    this.idCounter = maxId + 1;
+                }
+            } catch (IOException e) {
+                System.out.println("[Ошибка] при чтении файла: " + e.getMessage());
+            }
+        }
     }
 }
